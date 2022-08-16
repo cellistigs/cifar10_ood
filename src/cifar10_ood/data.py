72f1c9f7152f846e6a2305a2f2b22f630fe7f003
Ubuntu <ubuntu@ip-172-31-76-55.ec2.internal>
2022-08-16 18:21:53 +0000

    restored augmentations.

--- src/cifar10_ood/data.py
+++ src/cifar10_ood/data.py
@@ -444,8 +444,8 @@ class CIFAR10Data(pl.LightningDataModule):
         if aug is True:
             transform = T.Compose(
                 [
-                    #T.RandomCrop(32, padding=4),
-                    #T.RandomHorizontalFlip(),
+                    T.RandomCrop(32, padding=4),
+                    T.RandomHorizontalFlip(),
                     T.ToTensor(),
                     T.Normalize(self.mean, self.std),
                 ]
