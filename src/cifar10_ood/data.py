0b5f22e810b2c84f2f5f9ae3c311f475ead14d69
Taiga Abe <taigaabe@Taigas-MacBook-Air.local>
2023-03-28 21:23:34 -0400

    added bagging

--- src/cifar10_ood/data.py
+++ src/cifar10_ood/data.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytorch_lightning as pl
 import requests
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,Subset
 from torchvision import transforms as T
 import torchvision
 from torchvision.datasets import CIFAR10,CIFAR100,MNIST
@@ -495,6 +495,50 @@ class CIFAR10Data(pl.LightningDataModule):
 
     def test_dataloader(self):
         return self.val_dataloader()
+
+class CIFAR10_BagData(CIFAR10Data):
+    """CIFAR10 dataset, but with bagging. 
+
+    """
+    def __init__(self, args):
+        super().__init__()
+
+    def train_dataloader(self,shuffle = True,aug=True,bag_seed = None):
+        """bag.
+        """
+        if aug is True:
+            transform = T.Compose(
+                [
+                    T.RandomCrop(32, padding=4),
+                    T.RandomHorizontalFlip(),
+                    T.ToTensor(),
+                    T.Normalize(self.mean, self.std),
+                ]
+            )
+        else:    
+            transform = T.Compose(
+                [
+                    T.ToTensor(),
+                    T.Normalize(self.mean, self.std),
+                ]
+            )
+        dataset = CIFAR10(root=self.hparams.data_dir, train=True, transform=transform,download = True)
+        nb_datapoints = len(dataset.data)
+        indices = np.random.choice(np.arange(nb_datapoints),size = nb_datapoints)
+        if self.set_targets_train is not None:
+            dataset.targets = self.set_targets_train 
+            assert len(dataset.data) == len(dataset.targets), "number of examples, {} does not match targets {}".format(len(dataset.data),len(dataset.targets))
+            assert dataset.data.shape[1] >= np.max(dataset.targets), "number of classes, {} does not match target index {}".format(dataset.data.shape[1],np.max(dataset.targets)) 
+            
+        dataloader = DataLoader(
+            Subset(dataset,indices),
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            shuffle=shuffle,
+            drop_last=False,
+            pin_memory=True,
+        )
+        return dataloader
 
 class MNISTData(pl.LightningDataModule):
     def __init__(self, args):
